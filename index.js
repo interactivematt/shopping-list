9c8970abecef8ccab4edfbde45808abbfac16a44
Matt Castillo <mattcastillo2010@gmail.com>
2020-10-20 00:22:58 -0700

    Ready to submit
    Added form event listener, which creates an <li> for each newly added item. Also added event delegation to the check and delete functions, so those actions work dynamically.

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 function shoppingList() {
     // Here is how items get added
 
-
+    $('#js-shopping-list-form').submit( event => {
+        event.preventDefault();
+        let item = $('#shopping-list-entry').val();
+        $('ul').append('<li><span class="shopping-item">' + item + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+    });
 
     // Here is how items get checked
 
-    $('ul').find('.shopping-item-toggle').on('click', function(event) {
-        $(this).closest('li').find('.shopping-item').addClass('shopping-item__checked');
+    $('ul').on('click', '.shopping-item-toggle', function(event) {
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
     });
 
     // Here is how items get deleted 
 
-    $('ul').find('.shopping-item-delete').on('click', function(event) {
-        $(this).closest('li').remove('');
+    $('ul').on('click', '.shopping-item-delete', function(event) {
+        $(this).closest('li').remove();
     });
-
-
+    
 };
 
 $(shoppingList);
